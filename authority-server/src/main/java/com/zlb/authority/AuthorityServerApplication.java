bce4d53f6522ca8f25452a55a9858846d9f7c339
WenFeng1314 <839541645@qq.com>
2020-01-08 21:47:09 +0800

    添加

---
+++ authority-server/src/main/java/com/zlb/authority/AuthorityServerApplication.java
@@ -0,0 +1,13 @@
+package com.zlb.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityServerApplication.class, args);
+    }
+
+}
